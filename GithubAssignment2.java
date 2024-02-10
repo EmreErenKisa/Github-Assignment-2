cee7eeda03c91f9d515cb8367d76565132b00c71
EmreErenKisa <145989043+EmreErenKisa@users.noreply.github.com>
2024-02-10 18:15:48 +0300

    Update GithubAssignment2.java
    Added methods for finding the minimum/maximum numbers

--- GithubAssignment2.java
+++ GithubAssignment2.java
@@ -1,1 +1,31 @@
+/**
+* Method to find the smallest integer in the list of integers
+* @param numberList - the list of numbers
+* @return the smallest integer in the list
+*/
+public static int findMinimum(int[] numberList) {
+  int smallestNumber = numberlist[0];
 
+  for(int i = 1; i < numberList.length; i++) {
+    if(numberList[i] <= smallestNumber) {
+      smallestNumber = numberList[i];
+    }
+  }
+  return smallestNumber;
+}
+
+/**
+* Method to find the biggest integer in the list of integers
+* @param numberList - the list of numbers
+* @return the biggest integer in the list
+*/
+public static int findMaximum(int[] numberList) {
+  int biggestNumber = numberlist[0];
+
+  for(int i = 1; i < numberList.length; i++) {
+    if(numberList[i] >= biggestNumber) {
+      biggestNumber = numberList[i];
+    }
+  }
+  return biggestNumber;
+}
