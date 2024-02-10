de80859247332c20bbdd14c09cf7fecaf28d79ad
Ozan Atamer <146479979+OzanAtamer@users.noreply.github.com>
2024-02-10 18:18:31 +0300

    Added a function to find the sum of elements with odd and even-numbered indexes
    Added a function that sums the elements with the odd-numbered indexes and sums the elements with the even-numbered indexes. Then returns the sums as a string.

--- GithubAssignment2.java
+++ GithubAssignment2.java
@@ -1,1 +1,21 @@
 
+//Method to find the sum of elements with odd and even numbered indexes
+public static String oddEven(int[] array)
+{
+    int oddCount  = 0;
+    int evenCount = 0;
+    int arraySize = array.length;
+    for(int i = 0; i<arraySize;i++)
+    {
+        if(i % 2 == 0)
+        {
+            evenCount += array[i];
+        }
+        else
+        {
+            oddCount += array[i];
+        }
+    }
+    String counts = "Sum of the elements with odd indexes: " + oddCount + ". Sum of the elements with even indexes: " + evenCount;
+    return counts;
+}
