145f5c419844a6083c870a93fdf52464c75bb65c
Ozan <146588162+OzanAykut@users.noreply.github.com>
2024-02-10 18:27:56 +0300

    Update GithubAssignment2.java
    I wrote a static method that takes an Array of integers as an input. After that I used a loop to find the sum of the terms of the array and divided them to the term number to find the average. Then I used another loop to find the differences between the terms and the average and stored them in an array. Lastly I displayed the array of differences

--- GithubAssignment2.java
+++ GithubAssignment2.java
@@ -1,13 +1,15 @@
-public static void average( int[] list){
-  int sumOfNumbers = 0;
-  int average = 0;
-  int[] difference;
-  for( int i : list){
-  sumOfNumbers += i ;
+public class GithubAssignment2{
+  public static void average( int[] list){
+    int sumOfNumbers = 0;
+    int average = 0;
+    int[] difference;
+    for( int i : list){
+    sumOfNumbers += i ;
+    }
+    average = sumOfNumbers / list.length ;
+    for(int i = 0; i<list.length ; i++){
+      difference[i] = list[i] - average;
+    }
+    System.out.println(Arrays.toString(list));
   }
-  average = sumOfNumbers / list.length ;
-  for(int i = 0; i<list.length ; i++){
-    difference[i] = list[i] - average;
-  }
-  System.out.println(Arrays.toString(list));
 }
