20d76f2698904acc243a164a790d47db833a3c4b
DorukArca <146343805+DorukArca@users.noreply.github.com>
2024-02-10 18:27:04 +0300

    Update GithubAssignment2.java

--- GithubAssignment2.java
+++ GithubAssignment2.java
@@ -1,19 +1,22 @@
-public static String toString(int[] numbers)
-{
-    String str = "";
-    for (int i : numbers) 
+public class Github-Assignment-2{
+
+    public static String toString(int[] numbers)
     {
-        str += " " +i;
-    }
-    return str;
-}
-
-public static void createArray(int size){
-    int list[] = new int[size];
-
-        for( int i =0; i< size; i++)
+        String str = "";
+        for (int i : numbers) 
         {
-            int number = (int)(Math.random()*100);
-            list[i] = number;
+            str += " " +i;
         }
+        return str;
+    }
+    
+    public static void createArray(int size){
+        int list[] = new int[size];
+    
+            for( int i =0; i< size; i++)
+            {
+                int number = (int)(Math.random()*100);
+                list[i] = number;
+            }
+    }
 }
