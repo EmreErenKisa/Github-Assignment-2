8f10e3a3fa1acb8ade7afa87ed18d9accdddf7c2
EmreErenKisa <145989043+EmreErenKisa@users.noreply.github.com>
2024-02-10 18:26:35 +0300

    Update GithubAssignment2.java
    Put the methods inside class

--- GithubAssignment2.java
+++ GithubAssignment2.java
@@ -1,31 +1,33 @@
-/**
-* Method to find the smallest integer in the list of integers
-* @param numberList - the list of numbers
-* @return the smallest integer in the list
-*/
-public static int findMinimum(int[] numberList) {
-  int smallestNumber = numberlist[0];
-
-  for(int i = 1; i < numberList.length; i++) {
-    if(numberList[i] <= smallestNumber) {
-      smallestNumber = numberList[i];
+public GithubAssignment2 {
+  /**
+  * Method to find the smallest integer in the list of integers
+  * @param numberList - the list of numbers
+  * @return the smallest integer in the list
+  */
+  public static int findMinimum(int[] numberList) {
+    int smallestNumber = numberlist[0];
+  
+    for(int i = 1; i < numberList.length; i++) {
+      if(numberList[i] <= smallestNumber) {
+        smallestNumber = numberList[i];
+      }
     }
+    return smallestNumber;
   }
-  return smallestNumber;
-}
-
-/**
-* Method to find the biggest integer in the list of integers
-* @param numberList - the list of numbers
-* @return the biggest integer in the list
-*/
-public static int findMaximum(int[] numberList) {
-  int biggestNumber = numberlist[0];
-
-  for(int i = 1; i < numberList.length; i++) {
-    if(numberList[i] >= biggestNumber) {
-      biggestNumber = numberList[i];
+  
+  /**
+  * Method to find the biggest integer in the list of integers
+  * @param numberList - the list of numbers
+  * @return the biggest integer in the list
+  */
+  public static int findMaximum(int[] numberList) {
+    int biggestNumber = numberlist[0];
+  
+    for(int i = 1; i < numberList.length; i++) {
+      if(numberList[i] >= biggestNumber) {
+        biggestNumber = numberList[i];
+      }
     }
+    return biggestNumber;
   }
-  return biggestNumber;
 }
